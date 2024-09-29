90642472836c47067a506f80b23367825c065c43
ABIR <hbk.abir315@gmail.com>
2024-09-29 18:50:27 +0600

    [Added] Initial Frontend

---
+++ ecommerce-ui/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-10">
+
+      <h1 className="text-3xl text-orange-500">Ecommerce-UI</h1>
+
+    </div>
+  )
+}
+
+export default App
